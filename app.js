0e508b264c767055a66c5d39d201f023b2b98bc6
naquiyaasghar <naquiya.asghar@mail.sadiq.ai>
2025-05-03 17:09:01 +0500

    build: initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+// const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api/users', userRoutes);
+app.get("/custom-healthcheck", (req, res) => {
+    res.send("Service is running successfully :)");
+  });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
